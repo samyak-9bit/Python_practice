2a07c9366d9e275a437c2158d73af80dbea4fe16
Samyak Patni <samyak.patni@ninebit.in>
2024-03-05 12:40:35 +0530

    Priority Queue created

--- priority_Queue.py
+++ priority_Queue.py
@@ -1,29 +1,53 @@
+import heapq
+
 class Priority_Queue:
 
     def __init__(self):
         self.arr = []
     
-    def enqueue(self,item,priority):
-        newItem = {
-            'item' : item,
-            'priority' : priority
-        }
-        self.arr.append(newItem)
-
+    #insert at its corresponding position acc to priority in queue
+    def enqueue(self, item, priority):
+        newItem = (priority, item)
+        heapq.heappush(self.arr, newItem)
+    
+    #dequeue the element with highest priority
     def dequeue(self):
-        if(len(self.arr) == 0):
+        if len(self.arr) == 0:
             print("Queue is Empty")
         else:
-            return self.arr.pop(0)
-        
+            return heapq.heappop(self.arr)
+
+    # #peek the element with highest priority   
     def peek(self):
-        if(len(self.arr) == 0):
+        if len(self.arr) == 0:
             print("Queue is Empty")
         else:
             return self.arr[0]
     
     def print_Queue_basedOn_Priority(self):
-        sorted_Arr = self.arr.sort(True,key = lambda x:x['priority'])
-        print(sorted_Arr)
-        
-    
+        temp = self.arr.copy()
+        while len(temp)!= 0:
+            print(heapq.heappop(temp))
+
+q1 = Priority_Queue()
+q1.enqueue('Make coffee', 4)
+q1.enqueue('Make Tea', 2)
+q1.enqueue('Study', 1)
+q1.enqueue('Sing', 3)
+
+print("Initial Queue")
+q1.print_Queue_basedOn_Priority()
+
+print("Peek Element:")
+print(q1.peek())
+print("Queue after Peek:")
+q1.print_Queue_basedOn_Priority()
+
+print("Deque Element:")
+print(q1.dequeue())
+print("Queue after Dequeue:")
+q1.print_Queue_basedOn_Priority()
+
+
+
+
